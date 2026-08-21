5549a4027c5c269e68e3eafc52b0c7f4c03864e8
Roché Compaan <roche@upfrontsoftware.co.za>
2026-08-21 14:46:42 +0200

    fix: tighten repowolf client package check

--- nix/check-support/repowolf-client.nix
+++ nix/check-support/repowolf-client.nix
@@ -8,7 +8,19 @@ in
 pkgs.runCommand "repowolf-client-closure-check" { } ''
   set -eu
 
+  expected_bin_entries="$(printf '%s\n' gh repowolf-client repowolf-git-ssh)"
+  actual_bin_entries="$(find ${repowolfClient}/bin -mindepth 1 -maxdepth 1 -printf '%f\n' | sort)"
+  if test "$actual_bin_entries" != "$expected_bin_entries"; then
+    echo "unexpected RepoWolf client bin entries:" >&2
+    printf '%s\n' "$actual_bin_entries" >&2
+    exit 1
+  fi
+
   test -f ${repowolfClient}/bin/repowolf-client
+  if ! test ! -L ${repowolfClient}/bin/repowolf-client; then
+    echo "RepoWolf client executable must not be a symlink" >&2
+    exit 1
+  fi
   test -x ${repowolfClient}/bin/repowolf-client
   test "$(readlink ${repowolfClient}/bin/gh)" = repowolf-client
   test "$(readlink ${repowolfClient}/bin/repowolf-git-ssh)" = repowolf-client
